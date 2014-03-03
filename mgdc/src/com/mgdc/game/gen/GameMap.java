44a3ddf63fc07ecc1fa7d0937dbe9ca3e88ff14b
ShanTulshi <shan.tulshi@gmail.com>
2014-03-02 22:08:31 -0800

    new commit

--- mgdc/src/com/mgdc/game/gen/GameMap.java
+++ mgdc/src/com/mgdc/game/gen/GameMap.java
@@ -1,19 +1,40 @@
 package com.mgdc.game.gen;
 
+import com.mgdc.game.*;
 import java.util.*;
 public class GameMap
 {
 	protected ArrayList<ArrayList<Point>> map = new ArrayList<ArrayList<Point>>();
-	protected Integer startx = 0, starty = 0;
+	protected Integer originx = 51, originy = 51;
 	public GameMap()
 	{
-		for(int i = -50; i < 50; i++)
+		for(int i = -50; i < 50; i++)	//50 is an arbitrary starting value
 		{
 			map.add(new ArrayList<Point>());
-			for(int j = -50; j < 51; j++)
+			for(int j = -50; j < 50; j++)
 			{
-				map.get(i).add(new Point(this, i,j));
+				map.get(i).add(new Point(this, i,j));	//now we have a 100x100 square grid, with the origin at (51,51).
 			}
+		}
+	}
+	
+	public void addInDirection(Direction d)		//Will do later when my brain is working.
+	{
+		if(d == Direction.NORTH || d == Direction.NORTHEAST || d == Direction.NORTHWEST)
+		{
+			originy++;	//I know this needs to be here though.
+		}
+		if(d == Direction.SOUTH || d == Direction.SOUTHEAST || d == Direction.SOUTHWEST)
+		{
+			originy--;
+		}
+		if(d == Direction.EAST || d == Direction.SOUTHEAST || d == Direction.NORTHEAST)
+		{
+			originx--;
+		}
+		if(d == Direction.WEST || d == Direction.NORTHWEST || d == Direction.SOUTHWEST)
+		{
+			originx++;
 		}
 	}
 		
@@ -22,7 +43,7 @@ public class GameMap
 		
 	}
 	
-	public void refreshPosition(Point p)
+	public void refreshPosition(Point p) //Should put the Point p in the correct position in the ArrayList
 	{
 		
 	}
