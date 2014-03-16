f8be806d825b06fbe1d6418a11881240ff3cc6ff
ShanTulshi <shan.tulshi@gmail.com>
2014-03-16 12:03:39 -0700

    Please do renderer @William
    Signed-off-by: ShanTulshi <shan.tulshi@gmail.com>

--- mgdc/src/com/mgdc/game/gen/Renderer.java
+++ mgdc/src/com/mgdc/game/gen/Renderer.java
@@ -1,5 +1,17 @@
 package com.mgdc.game.gen;
 
-public class Renderer {
+import java.lang.IllegalArgumentException;
 
+public class Renderer {
+	public int[][] window = new int[2][2];
+	public void setWindow (int[][] in) throws IllegalArgumentException
+	{
+		if(in.length != 0) throw new IllegalArgumentException("The window passed has the wrong dimensions.");
+		if(in[0].length != 0) throw new IllegalArgumentException("The window passed has the wrong dimensions.");
+		if(in[1].length != 0) throw new IllegalArgumentException("The window passed has the wrong dimensions.");
+		window = in;
+	}
+	public void render(int x, int y){
+		//Implement pls William
+	}
 }
